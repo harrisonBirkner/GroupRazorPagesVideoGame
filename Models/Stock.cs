15c094ca26da9602950cbd01d3719f8b3c3a6a26
Boliver19 <57954561+Boliver19@users.noreply.github.com>
2021-02-16 23:50:53 -0600

    Model

---
+++ Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace GroupRazorPagesVideoGame.Models
+{
+	public class Stock
+	{
+		public int ID { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+	}
+}
